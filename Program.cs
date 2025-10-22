c6b0cec2de0158f13e966c9d9e9f413650bfa13a
saw <saw>
2025-10-22 21:06:30 +1300

    update website

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
+
 // Identity + default UI
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
